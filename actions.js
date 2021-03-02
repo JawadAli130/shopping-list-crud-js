2c076a3282c24236b33fa10bba3c2bdd1d712a0d
Jawad Ali <jawadhaiderqadri@gmail.com>
2021-03-02 21:17:04 +0500

    update button continue

--- actions.js
+++ actions.js
@@ -2,31 +2,51 @@ var input = document.getElementById("txtInput");
 var enterBtn = document.getElementById("enterBtn");
 var list = document.querySelector("ul");
 var delBtn = document.getElementsByClassName("del");
+var updateBtn = document.getElementsByClassName("update");
 var clearBtn = document.getElementById("clearBtn");
+
+for(var i=0;i<updateBtn.length;i++){
+    updateBtn[i].onclick=updateContent;
+}
+
 for(var i=0;i<delBtn.length;i++){
     delBtn[i].onclick=removeLi;
 }
 
+function updateContent(e){
+    console.log(e.target.parentNode.firstChild);
+}
+
+//strike through function
 function strikeThrough(e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("done");
     }
 }
 
+//deleting a list item
 function removeLi(evt){
 	evt.target.parentNode.remove();
 } 
 
-
+//creating new li element
 function createListElement(){
     var li = document.createElement("li");
+
     var btn = document.createElement("button");
+    var upBtn = document.createElement("button");
+
+    upBtn.appendChild(document.createTextNode("Update"));
     btn.appendChild(document.createTextNode("Delete"));
+
     li.appendChild(document.createTextNode(input.value));
     li.appendChild(btn);
+    li.appendChild(upBtn);
+
     list.appendChild(li);
     input.value="";
     btn.onclick=removeLi;
+    upBtn.onclick=updateContent;
 }
 
 function addListAftercClick(){
@@ -39,6 +59,7 @@ function addListAfterPress(event){
         createListElement();
     }
 }
+//clearng list
 function clearList(){
     list.innerHTML="";
 }
